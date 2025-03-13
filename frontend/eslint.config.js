51551fd414f47e93f6ef04fa75939fe9e5981ff5
VictoriaRodik <victoriarodik@gmail.com>
2025-03-13 17:00:06 +0200

    Remove .vite/deps from repository

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,15 +23,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-explicit-any': 'off',
     },
-    overrides: [
-      {
-        files: ['**/*.test.ts', '**/*.test.tsx'],
-        rules: {
-          '@typescript-eslint/no-explicit-any': 'off'
-        }
-      }
-    ]
   },
 )
