eecf69568c282d910d58390f67a4632c407e3563
VictoriaRodik <victoriarodik@gmail.com>
2025-03-13 17:18:56 +0200

    Modified:   eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist'],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ...js.configs.recommended,
+    ...tseslint.configs.recommended,
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -26,8 +27,9 @@ export default [
     },
   },
   {
-    files: ['**/*.test.ts', '**/*.test.tsx'],
+    files: ['**/*.test.{ts,tsx}', '**/*.spec.{ts,tsx}'],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',     },
-  },
-];
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
+);
